f3259d3f9e8275c85268b07c1914b7056d965d8a
odkken <odkken@gmail.com>
2015-08-24 23:41:20 -0700

    host/join server buttons "work"

--- Assets/Scripts/ServerActionButton.cs
+++ Assets/Scripts/ServerActionButton.cs
@@ -3,12 +3,22 @@ using UnityEngine;
 
 namespace Assets.Scripts
 {
-    public class JoinButton : MonoBehaviour
+    public class ServerActionButton : MonoBehaviour
     {
         public Matchmaker Matchmaker;
         public SimpleInput ServerNameInput;
         public SimpleInput PlayerNameInput;
-        public void Click()
+        public void Host()
+        {
+            if (!ServerNameInput.IsFilledOut)
+                Debug.Log("Please specify a server name!");
+            else if (!PlayerNameInput.IsFilledOut)
+                Debug.Log("Your name cannot be empty!");
+            else
+                Matchmaker.StartServer(PlayerNameInput.Text, ServerNameInput.Text);
+        }
+
+        public void Join()
         {
             if (!ServerNameInput.IsFilledOut)
                 Debug.Log("Please specify a server name!");
